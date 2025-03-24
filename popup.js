1a173f2b7c2f6b0265b4918cab7d06fc92b9dcd7
Bill O'Hanlon <ohanlonbill1998@gmail.com>
2025-03-24 13:29:05 +0000

    Updating popup to use grid

--- popup.js
+++ popup.js
@@ -1,24 +1,35 @@
 document.getElementById('fetchJobs').addEventListener('click', fetchJobs);
 
 function fetchJobs() {
-    var ul = document.getElementById("jobs-list");
-    ul.innerHTML = "";
+    var tbody = document.getElementById("jobs-rows");
+    tbody.innerHTML = "";
 
     chrome.storage.local.getKeys((keys) => {
         for (var i = 0; i < keys.length; i++) {
-            var li = document.createElement('li');
-            li.setAttribute('id', keys[i]);
-            ul.appendChild(li);
-            queryData(keys[i]);
+            var tr = document.createElement('tr');
+            tr.setAttribute('id', keys[i]);
+            tbody.appendChild(tr);
+
+            applyFunctionToJobValue(keys[i], function(result) {
+                let key = Object.keys(result)[0];
+                var tr = document.getElementById(key);
+                tr.innerHTML = convertJobObjToLiHTML(result[key]);
+            });
         }
     });
 }
 
-function queryData(jobId) {
-    return chrome.storage.local.get([jobId], function(result) {
-        console.log(result[jobId].title);
-        var title = new String(result[jobId].title);
-        var li = document.getElementById(jobId);
-        li.innerText = title;
-    });
+function applyFunctionToJobValue(jobId, func) {
+    chrome.storage.local.get([jobId], func);
+}
+
+function convertJobObjToLiHTML(jobObj) {
+    let titleField = '<td>' + jobObj.title + '</td>';
+    let companyField = '<td>' + jobObj.company + '</td>';
+    let locationField = '<td>' + jobObj.location + '</td>';
+    let dateSaved = '<td>' + jobObj.dateSaved + '</td>';
+    let appliedCheckbox = '<td><input type="checkbox" id="applied-checkbox-' + jobObj.jobId + '"></td>';
+    let rejectedCheckbox = '<td><input type="checkbox" id="rejected-checkbox-' + jobObj.jobId + '"></td>';
+
+    return titleField + companyField + locationField + dateSaved + appliedCheckbox + rejectedCheckbox;
 }
